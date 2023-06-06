5748dcaf72cf4dc09cbbcf18c95d749055095b93
woneze <00wonjae@gmail.com>
2023-06-06 22:11:12 +0900

    else

--- js/Conditionals/app.js
+++ js/Conditionals/app.js
@@ -1,22 +1,21 @@
 // let random = Math.random();
 // if (random < 0.5) {
 //   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-//   console.log(random);
+// } else {
+//   console.log("YOUR NUMBER IS GREATER (OR EQUAL) THAN 0.5!!!");
 // }
+// console.log(random);
 
-// if (random >= 0.5) {
-//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-//     console.log(random);
-//   }
+// const dayOfWeek = prompt("ENTER A DAY").toLowerCase();
 
-// const dayOfWeek = "Monday";
-
-// if (dayOfWeek === "Monday") {
+// if (dayOfWeek === "monday") {
 //   console.log("UGHHH I HATE MONDAYS!");
-// } else if (dayOfWeek === "Saturday") {
+// } else if (dayOfWeek === "saturday") {
 //   console.log("YAY I LOVE SATURDAYS!");
-// } else if (dayOfWeek === "Friday") {
+// } else if (dayOfWeek === "friday") {
 //   console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK.");
+// } else {
+//   console.log("MEH");
 // }
 
 const age = 8;
@@ -24,7 +23,9 @@ const age = 8;
 if (age < 5) {
   console.log("You are a baby. You get in for free!");
 } else if (age < 10) {
-  console.log('You are a child. You pay $10')
+  console.log("You are a child. You pay $10");
 } else if (age < 65) {
-  console.log('You are an adulut. You pay $20')
+  console.log("You are an adulut. You pay $20");
+} else {
+  console.log("You are a senior. You pay $10");
 }
