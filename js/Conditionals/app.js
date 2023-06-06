193fa35f7b3beea7bceefae06d7efafe9191190e
woneze <00wonjae@gmail.com>
2023-06-06 18:32:39 +0900

    else-if

--- js/Conditionals/app.js
+++ js/Conditionals/app.js
@@ -1,10 +1,30 @@
-let random = Math.random();
-if (random < 0.5) {
-  console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-  console.log(random);
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//   console.log(random);
+// }
 
-if (random >= 0.5) {
-    console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-    console.log(random);
-  }
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+//     console.log(random);
+//   }
+
+// const dayOfWeek = "Monday";
+
+// if (dayOfWeek === "Monday") {
+//   console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === "Saturday") {
+//   console.log("YAY I LOVE SATURDAYS!");
+// } else if (dayOfWeek === "Friday") {
+//   console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK.");
+// }
+
+const age = 8;
+
+if (age < 5) {
+  console.log("You are a baby. You get in for free!");
+} else if (age < 10) {
+  console.log('You are a child. You pay $10')
+} else if (age < 65) {
+  console.log('You are an adulut. You pay $20')
+}
