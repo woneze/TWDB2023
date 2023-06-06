e789a486395a5711156682dcb3e72ad43faa8cf4
woneze <00wonjae@gmail.com>
2023-06-07 00:42:30 +0900

    logical or

--- js/Logical_Operators/app.js
+++ js/Logical_Operators/app.js
@@ -1,7 +1,18 @@
-const password = prompt("Enter your password");
+// const password = prompt("Enter your password");
 
-if(password.length >= 6 && password.indexOf(' ') === -1) {
-    console.log("Valid Password!")
+// if(password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("Valid Password!")
+// } else {
+//     console.log("Incorrect Format For Password!")
+// }
+
+const age = 90;
+if (age >= 0 && age < 5 || age >= 65) {
+  console.log("FREE");
+} else if (age >= 5 && age < 10) {
+  console.log("$10");
+} else if (age >= 10 && age < 65) {
+  console.log("$20");
 } else {
-    console.log("Incorrect Format For Password!")
+    console.log("INVALID AGE!")
 }
