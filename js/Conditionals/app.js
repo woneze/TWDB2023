bfc17c01b3cafeb539c227fdc3ea68bafe41afcf
woneze <00wonjae@gmail.com>
2023-06-06 22:24:16 +0900

    nesting

--- js/Conditionals/app.js
+++ js/Conditionals/app.js
@@ -18,14 +18,26 @@
 //   console.log("MEH");
 // }
 
-const age = 8;
+// const age = 8;
 
-if (age < 5) {
-  console.log("You are a baby. You get in for free!");
-} else if (age < 10) {
-  console.log("You are a child. You pay $10");
-} else if (age < 65) {
-  console.log("You are an adulut. You pay $20");
+// if (age < 5) {
+//   console.log("You are a baby. You get in for free!");
+// } else if (age < 10) {
+//   console.log("You are a child. You pay $10");
+// } else if (age < 65) {
+//   console.log("You are an adulut. You pay $20");
+// } else {
+//   console.log("You are a senior. You pay $10");
+// }
+
+const password = prompt("please enter a new password");
+
+if (password.length >= 6) {
+  if (password.indexOf(" ") === -1) {
+    console.log("Valid Password!");
+  } else {
+    console.log("Password cannot contain spaces!");
+  }
 } else {
-  console.log("You are a senior. You pay $10");
+  console.log("PASSWORD TOO SHORT! Must be 6+ characters");
 }
