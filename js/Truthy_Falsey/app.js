8694af91a1d48ce59c2685fb988becb62693d49c
woneze <00wonjae@gmail.com>
2023-06-07 00:29:07 +0900

    truthy_falsey

---
+++ js/Truthy_Falsey/app.js
@@ -0,0 +1,13 @@
+// const userInput = prompt("Enter something");
+
+// if (userInput) {
+//     console.log("TRUTHY!");
+// } else {
+//     console.log("FALSY!");
+// }
+
+if (' ') {
+  console.log("TRUTHY");
+} else {
+  console.log("FALSY");
+}
