5499ce77cc48e6940deb30627d0e7579b90d41db
eaj <00wonjae@gmail.com>
2023-06-08 16:29:28 +0900

    loop game

--- js/While_Loops/app.js
+++ js/While_Loops/app.js
@@ -19,7 +19,34 @@
 // }
 // console.log("OK YOU WIN!");
 
-for (let i = 0; i < 1000; i++) {
-    console.log(i);
-    if (i === 100) break;
+// for (let i = 0; i < 1000; i++) {
+//     console.log(i);
+//     if (i === 100) break;
+// }
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess :");
+    } else {
+        guess = prompt("Too low! Enter a new guess : ");
+    }
+}
+if(guess === 'q') {
+    console.log("OK, YOU QUIT!")
+} else {
+    console.log("CONGRATS YOU WIN!")
+    console.log(`You got it! It took you ${attempts} guesses`)
 }
