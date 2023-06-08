3fd091754e51611bf05877bae998fa2ae0cab58c
eaj <00wonjae@gmail.com>
2023-06-08 16:11:16 +0900

    while loops

---
+++ js/While_Loops/app.js
@@ -0,0 +1,25 @@
+// let count = 0;
+// while (count <10) {
+//     count++;
+//     console.log(count);
+// }
+
+// const SECRET = "BabyHippo";
+
+// let guess = prompt("enter the secret code...");
+// while(guess !== SECRET) {
+//     guess = prompt("enter the secret code...");
+// }
+// console.log("CONGRATS! YOU GOT THE SECRET!!!");
+
+// let input = prompt("Hey, say something!");
+// while (true) {
+//     input = prompt(input);
+//     if(input === "stop copying me") break;
+// }
+// console.log("OK YOU WIN!");
+
+for (let i = 0; i < 1000; i++) {
+    console.log(i);
+    if (i === 100) break;
+}
