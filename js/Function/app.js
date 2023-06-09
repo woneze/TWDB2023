b6b024cb57ed5d579a23f658b71362f9116b56da
eaj <00wonjae@gmail.com>
2023-06-09 18:57:39 +0900

    function init

---
+++ js/Function/app.js
@@ -0,0 +1,13 @@
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+function greet(firstName) {
+    console.log(`Hey there, ${firstName}!`);
+}
+// firstName은 palceholder와 비슷한 역할을 하고 있음
+// firstName = 매개변수
+// firstName의 위치에 임의의 값을 넣어 함수 내부로 전달하게 되면
+// 그 임의의 값을 인수라고 함.
