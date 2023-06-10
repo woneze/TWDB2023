0ed64a18806a928b078006db96fa6756ec059074
woneze <00wonjae@gmail.com>
2023-06-11 01:42:03 +0900

    block scope

---
+++ js/Scope/app.js
@@ -0,0 +1,30 @@
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     // let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+// birdWatch();
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'HIII!'
+// }
+
+// console.log(radius);
+// console.log(msg);
+
+for (let i = 0; i < 5; i++) {
+    let msg = 'sdasdsdas';
+    console.log(msg);
+}
+console.log(msg)
+console.log(i)
