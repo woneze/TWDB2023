bf1a055e98efeec61a8a1447897f440f87101116
woneze <00wonjae@gmail.com>
2023-06-11 03:13:56 +0900

    try / catch

---
+++ js/Function/TryCatch/app.js
@@ -0,0 +1,13 @@
+try {
+  hello.toUpperCase();
+} catch {
+  console.log("ERROR!!!");
+}
+
+function yell(msg) {
+  try {
+    console.log(msg.toUpperCase().repeat(3));
+  } catch (e) {
+    console.log("Please pass a string next time!");
+  }
+}
