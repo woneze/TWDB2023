1df5814e735abc01a5f639a9f3542226d355c169
eaj <00wonjae@gmail.com>
2023-06-12 23:00:25 +0900

    arrowfunction fin

--- js/ArrayCallbackMethods/ArrowFunctions/app.js
+++ js/ArrayCallbackMethods/ArrowFunctions/app.js
@@ -14,9 +14,7 @@ const square = (num) => {
 //   return Math.floor(Math.random() & 6) + 1;
 // };
 
-const rollDie = () => (
-  Math.floor(Math.random() & 6) + 1
-);
+const rollDie = () => Math.floor(Math.random() & 6) + 1;
 
 const add = (a, b) => a + b;
 
@@ -25,7 +23,7 @@ const add = (a, b) => a + b;
 // } // regular function expression
 
 // const isEven = (num) => {
-//   return num % 2 === 0; 
+//   return num % 2 === 0;
 // } // arrow function with parens around param
 
 // const isEven = num => {
@@ -36,6 +34,31 @@ const add = (a, b) => a + b;
 //   num % 2 === 0
 // ); // implicit return
 
-const isEven = num => num % 2 === 0;
+const isEven = (num) => num % 2 === 0;
 // one-liner implicit return
 // 깔끔한 표현식 단 하나로 유일한 값을 반환할 경우에만 사용함.
+
+const movies = [
+  {
+    title: "Amadeus",
+    score: 99,
+  },
+  {
+    title: "Stand By Me",
+    score: 85,
+  },
+  {
+    title: "Parasite",
+    score: 95,
+  },
+  {
+    title: "Alien",
+    score: 90,
+  },
+];
+
+// const newMovies = movies.map(function (moive) {
+//   return `${movie.title} - ${movie.score} / 10`;
+// });
+
+const newMovies = movies.map((movie) => `${movie.title} - ${movie.score} / 10`);
