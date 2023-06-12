539e55789ddecfe05d1df43e3c02b69f1d0a56e7
eaj <00wonjae@gmail.com>
2023-06-12 15:37:01 +0900

    arrow functions

---
+++ js/ArrayCallbackMethods/ArrowFunctions/app.js
@@ -0,0 +1,15 @@
+// const add = function (x, y) {
+//   return x + y;
+// };
+
+const add = (x, y) => {
+  return x + y;
+};
+
+const square = (num) => {
+  return num * num;
+};
+
+const rollDie = () => {
+  return Math.floor(Math.random() & 6) + 1;
+};
