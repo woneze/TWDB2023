34777a5cfa6fb371481aa7393f02812806a4da2c
woneze <00wonjae@gmail.com>
2023-06-12 21:39:05 +0900

    implicit return

--- js/ArrayCallbackMethods/ArrowFunctions/app.js
+++ js/ArrayCallbackMethods/ArrowFunctions/app.js
@@ -2,14 +2,40 @@
 //   return x + y;
 // };
 
-const add = (x, y) => {
-  return x + y;
-};
+// const add = (x, y) => {
+//   return x + y;
+// };
 
 const square = (num) => {
   return num * num;
 };
 
-const rollDie = () => {
-  return Math.floor(Math.random() & 6) + 1;
-};
+// const rollDie = () => {
+//   return Math.floor(Math.random() & 6) + 1;
+// };
+
+const rollDie = () => (
+  Math.floor(Math.random() & 6) + 1
+);
+
+const add = (a, b) => a + b;
+
+// const isEven = function (num) {
+//   return num % 2 === 0;
+// } // regular function expression
+
+// const isEven = (num) => {
+//   return num % 2 === 0; 
+// } // arrow function with parens around param
+
+// const isEven = num => {
+//   return num % 2 === 0 ;
+// } // no parens around param
+
+// const isEven = num => (
+//   num % 2 === 0
+// ); // implicit return
+
+const isEven = num => num % 2 === 0;
+// one-liner implicit return
+// 깔끔한 표현식 단 하나로 유일한 값을 반환할 경우에만 사용함.
