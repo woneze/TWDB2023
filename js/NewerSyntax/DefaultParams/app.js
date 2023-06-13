aa713fae68f11df4b3ca0a069cec25720333ac18
eaj <00wonjae@gmail.com>
2023-06-13 16:06:46 +0900

    default params

---
+++ js/NewerSyntax/DefaultParams/app.js
@@ -0,0 +1,14 @@
+// function rollDie(numSides) {
+//   if (numSides === undefined) {
+//     numSides = 6;
+//   }
+//   return Math.floor(Math.random() * numSides) + 1;
+// }
+
+function rollDie(numSides = 6) {
+  return Math.floor(Math.random() * numSides) + 1;
+}
+
+function greet(person, msg = "Hey There", punc = "!") {
+  return `${msg}, ${person} ${punc}`;
+}
