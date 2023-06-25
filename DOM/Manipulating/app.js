1a077f9439929c249c017f90ca9b39622224c535
eaj <00wonjae@gmail.com>
2023-06-25 12:25:22 +0900

    style change

--- DOM/Manipulating/app.js
+++ DOM/Manipulating/app.js
@@ -1,5 +1,11 @@
 const allLinks = document.querySelectorAll("a");
 
+// for (let link of allLinks) {
+//   link.innerText = "I AM A LINK!!";
+// }
+
 for (let link of allLinks) {
-  link.innerText = "I AM A LINK!!";
+  link.style.color = "rgb(0, 108, 134)";
+  link.style.textDecorationColor = "magenta";
+  link.style.textDecorationStyle = "wavy";
 }
