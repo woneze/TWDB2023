65a190b629416e03ee94a7156c7ae5ecd90ee9c7
eaj <00wonjae@gmail.com>
2023-07-08 18:38:32 +0900

    addEvent

--- DOM/EventBasics/app.js
+++ DOM/EventBasics/app.js
@@ -15,3 +15,24 @@ btn.onmouseenter = scream;
 document.querySelector("h1").onclick = () => {
   alert("you clicked the h1!");
 };
+
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("click", function () {
+  alert("CLICKED!");
+});
+
+function twist() {
+  console.log("TWIST!");
+}
+
+function shout() {
+  console.log("SHOUT!");
+}
+
+const tasButton = document.querySelector("#tas");
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+tasButton.addEventListener("click", twist);
+tasButton.addEventListener("click", shout);
