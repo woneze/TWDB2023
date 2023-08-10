1a19a00a7ccda47f28a338e096065c336b522053
woneze <00wonjae@gmail.com>
2023-08-10 22:45:21 +0900

    비동기함수 오류 처리

--- Async/AsyncFunctions/app.js
+++ Async/AsyncFunctions/app.js
@@ -64,3 +64,28 @@ async function printRainbow() {
   await rainbow();
   console.log("END OF RAINBOW");
 }
+
+const fakeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 2000) {
+        reject("Connection Timeout :(");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
+};
+
+async function makeTwoRequests() {
+  try {
+    let data1 = await fakeRequest("/page1");
+    console.log(data1);
+    let data2 = await fakeRequest("/page2");
+    console.log(data2);
+  } catch (e) {
+    console.log("CAUGHT AN ERROR!");
+    console.log("error is :", e);
+  }
+}
