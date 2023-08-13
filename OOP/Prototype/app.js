a907dd940e82ea79af5c79f0a6a8ac7ab8246a19
woneze <00wonjae@gmail.com>
2023-08-13 23:45:23 +0900

    OOP - Prototype

---
+++ OOP/Prototype/app.js
@@ -0,0 +1,7 @@
+String.prototype.yell = function () {
+  return `OMG!!! ${this.toUpperCase()}!!!`;
+};
+
+Array.prototype.pop = function () {
+  return "SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!";
+};
