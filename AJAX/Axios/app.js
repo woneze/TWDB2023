e61d92b0581ab75380d8ba32b99df6873ab656bf
eaj <00wonjae@gmail.com>
2023-08-13 15:38:37 +0900

    setting header

--- AJAX/Axios/app.js
+++ AJAX/Axios/app.js
@@ -16,5 +16,27 @@ const getStarWarsPerson = async (id) => {
   }
 };
 
-getStarWarsPerson(5);
-getStarWarsPerson(10);
+// getStarWarsPerson(5);
+// getStarWarsPerson(10);
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+};
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
+  } catch (e) {
+    return "NO JOKE AVAILABLE! SORRY :(";
+  }
+};
+
+button.addEventListener("click", addNewJoke);
