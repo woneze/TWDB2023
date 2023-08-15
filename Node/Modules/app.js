aa05e67bb71eeea9a8d86118ff35d6cd03c0321e
woneze <00wonjae@gmail.com>
2023-08-15 21:09:23 +0900

    directory

--- Node/Modules/app.js
+++ Node/Modules/app.js
@@ -1,3 +1,7 @@
 const math = require("./math");
-console.log(math.PI);
-console.log(math.square(9));
+const cats = require("./Shelter");
+
+// console.log(math.PI);
+// console.log(math.square(9));
+
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats);
