eba9f4113e809dd89dfc69f415d254a5eda30e2f
woneze <00wonjae@gmail.com>
2023-08-16 19:15:06 +0900

    remove node_modules

--- Node/Modules/Artster/index.js
+++ Node/Modules/Artster/index.js
@@ -1,7 +1,7 @@
 const figlet = require("figlet");
 const colors = require("colors");
 
-figlet("20982", function (err, data) {
+figlet("Hello World", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
