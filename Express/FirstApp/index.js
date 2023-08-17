e8511beac7b1cb860151f609d5fa6d4a4a77349e
eaj <00wonjae@gmail.com>
2023-08-17 16:23:28 +0900

    Require & Request

--- Express/FirstApp/index.js
+++ Express/FirstApp/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
   console.log("WE GOT A NEW REQUEST!");
+  res.send("HELLO, WE GOT YOUR REQEST! THIS IS A RESPONSE!!");
 });
 
 app.listen(3000, () => {
