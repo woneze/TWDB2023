a8311e26449d23af19f5259c9406044bc820a853
eaj <00wonjae@gmail.com>
2023-08-18 16:14:20 +0900

    Express Routing Basic

--- Express/FirstApp/index.js
+++ Express/FirstApp/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  console.log("WE GOT A NEW REQUEST!");
-  res.send("HELLO, WE GOT YOUR REQEST! THIS IS A RESPONSE!!");
+// app.use((req, res) => {
+//   console.log("WE GOT A NEW REQUEST!");
+//   res.send("HELLO, WE GOT YOUR REQEST! THIS IS A RESPONSE!!");
+// });
+
+app.get("/", (req, res) => {
+  res.send("This is the home page!");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("POST REQUEST TO /cats!!");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("MEOW!!");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOF!!");
+});
+
+app.get("*", (req, res) => {
+  res.send(`I don't know that path!`);
 });
 
 app.listen(3000, () => {
